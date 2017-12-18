81aa5e3cf3f5dd182179ae6abe6e8cfe732a7e20
ullone <ullone@163.com>
2017-12-18 11:17:11 +0800

    fix

--- ams/public/spide/spide.php
+++ ams/public/spide/spide.php
@@ -78,157 +78,157 @@ class Spide {
   }
 
   //worker进程启动时执行
-  public function onWorkerStart() {
-    foreach($this->startWorkerHooks as $hook) {
-      call_user_func($hook, $this);
-    }
-  }
-
-  //worker进程停止时执行
-  public function onWorkerStop() {
-    foreach($this->stopWorkerHooks as $hook) {
-      call_user_func($hook, $this);
-    }
-  }
-
-  //初始化一系列钩子函 数并规定执行顺序
-  public function initHooks() {
-    //写日志
-    $this->startWorkerHooks[] = function ($spide) {
-        $spide->id = $spide->worker->id;
-        $spide->log("Beanbun worker {$spide->id} is starting ...");
-    };
-
-    //自定义处理函数
-    if ($this->startWorker) {
-        $this->startWorkerHooks[] = $this->startWorker;
-    }
-    //设置队列大小
-    $this->startWorkerHooks[] = function ($spide) {
-        $spide->queue()->maxQueueSize = $spide->max;
-        //定时调用一次$this->hooks里的函数，一次爬取五条url
-        $spide->timer_id = Spide::timer($spide->interval, [$spide, 'crawler']);
-    };
-
-    $this->beforeDownloadPageHooks[] = [$this, 'defaultBeforeDownloadPage'];
-
-    if($this->beforeDownloadPage) {
-      $this->beforeDownloadPageHooks[] = $this->beforeDownloadPage;
-    }
-
-    if($this->downloadPage) {
-      $this->downloadPageHooks[] = $this->downloadPage;
-    } else {
-      $this->downloadPageHooks[] = [$this, 'defaultDownloadPage'];
-    }
-
-    if($this->afterDownloadPage) {
-      $this->afterDownloadPageHooks[] = $this->afterDownloadPage;
-    }
-
-    if($this->discoverUrl) {
-      $this->discoverUrlHooks[] = $this->discoverUrl;
-    } else {
-      $this->discoverUrlHooks[] = [$this, 'defaultDiscoverUrl'];
-    }
-
-    if($this->afterDiscover) {
-      $this->afterDiscoverHooks[] = $this->afterDiscover;
-    }
-
-    if($this->daemonize) {
-      $this->afterDiscoverHooks[] = function ($spide) {
-        if($spide->options['reserve'] == false) {
-          $spide->queue()->queued($spide->queue);
-        }
-      };
-    }
-
-    if($this->stopWorker) {
-      $this->stopWorkerHooks[] = $this->stopWorker;
-    }
-  }
-
-  public static function timer($interval, $callBack, $args = [], $persistent = true) {
-    /*
-    * 调用php页面运行时间检测类，定时执行某个函数或者类方法。
-    * @param $interval   执行间隔
-    * @param $callback   执行函数，若是类的方法，必须是类的公有方法
-    * @param $args       回调函数的参数，必须是数组
-    * @param $persistent 是否持久，若只想执行一次则传false,只执行一次的任务在执行完毕后会自动销毁，不必调用Timer::del()
-    */
-    reuturn Timer::add($interval, $callBack, $args, $persistent);
-  }
-
-  public static function timerDel($timer_id) {
-    Timer::del($timer_id);
-  }
-
-  public static function run() {
-    Worker::runAll();
-  }
-
-  //命令行控制
-  public function command() {
-    switch($this->commands[1]) {
-      case 'start' :
-        foreach($this->seed as $url) {
-          if(is_string($url)) {
-            $this->queue()->add($url);
-          } elseif(is_array($url)) {
-            $this->queue()->add($url[0], $url[1]);
-          }
-        }
-        $this->queues = null;//初始化
-        $this->log('Spide is starting');
-        break;
-
-      case 'clean':
-        $this->queue->clean();
-        die();
-        break;
-      case 'stop':
-        break;
-      default:
-        break;
-    }
-  }
-
-  private function fileLog($msg, $spide) {
-    $myFile  = fopen($this->lofFile, "a") or die ('open file failed!');
-    $content = date('Y-m-d H:i:s')."$spide->name : $msg\n";
-    fwrite($myFile, $content);
-    fclose($myFile);
-  }
-
-  public function log($msg) {
-    call_user_func($this->logFactory, $msg, $this);
-  }
-
-  public function setLog($callBack = null) {
-    $this->logFactory = $callBack === null ? function($msg, $spide) {
-      echo date('Y-m-d H:i:s')."$spide->name : $msg\n";
-    } : $callBack;
-  }
-
-  //非守护进程下执行，一次爬取五条
-  public function crawler() {
-    $allHooks = $this->hooks;
-    array_shift($allHooks);
-    array_pop($allHooks);
-
-    foreach ($allHooks as $hooks) {//爬取5条记录
-        foreach ($this->$hooks as $hook) {//完成爬取一条记录的整个流程
-            call_user_func($hook, $this);
-        }
-    }
-
-    // $this->queue = '';
-    // $this->url = '';
-    // $this->method = '';
-    // $this->page = '';
-    // $this->options = [];
-  }
+  // public function onWorkerStart() {
+  //   foreach($this->startWorkerHooks as $hook) {
+  //     call_user_func($hook, $this);
+  //   }
+  // }
+  //
+  // //worker进程停止时执行
+  // public function onWorkerStop() {
+  //   foreach($this->stopWorkerHooks as $hook) {
+  //     call_user_func($hook, $this);
+  //   }
+  // }
+  //
+  // //初始化一系列钩子函 数并规定执行顺序
+  // public function initHooks() {
+  //   //写日志
+  //   $this->startWorkerHooks[] = function ($spide) {
+  //       $spide->id = $spide->worker->id;
+  //       $spide->log("Beanbun worker {$spide->id} is starting ...");
+  //   };
+  //
+  //   //自定义处理函数
+  //   if ($this->startWorker) {
+  //       $this->startWorkerHooks[] = $this->startWorker;
+  //   }
+  //   //设置队列大小
+  //   $this->startWorkerHooks[] = function ($spide) {
+  //       $spide->queue()->maxQueueSize = $spide->max;
+  //       //定时调用一次$this->hooks里的函数，一次爬取五条url
+  //       $spide->timer_id = Spide::timer($spide->interval, [$spide, 'crawler']);
+  //   };
+  //
+  //   $this->beforeDownloadPageHooks[] = [$this, 'defaultBeforeDownloadPage'];
+  //
+  //   if($this->beforeDownloadPage) {
+  //     $this->beforeDownloadPageHooks[] = $this->beforeDownloadPage;
+  //   }
+  //
+  //   if($this->downloadPage) {
+  //     $this->downloadPageHooks[] = $this->downloadPage;
+  //   } else {
+  //     $this->downloadPageHooks[] = [$this, 'defaultDownloadPage'];
+  //   }
+  //
+  //   if($this->afterDownloadPage) {
+  //     $this->afterDownloadPageHooks[] = $this->afterDownloadPage;
+  //   }
+  //
+  //   if($this->discoverUrl) {
+  //     $this->discoverUrlHooks[] = $this->discoverUrl;
+  //   } else {
+  //     $this->discoverUrlHooks[] = [$this, 'defaultDiscoverUrl'];
+  //   }
+  //
+  //   if($this->afterDiscover) {
+  //     $this->afterDiscoverHooks[] = $this->afterDiscover;
+  //   }
+  //
+  //   if($this->daemonize) {
+  //     $this->afterDiscoverHooks[] = function ($spide) {
+  //       if($spide->options['reserve'] == false) {
+  //         $spide->queue()->queued($spide->queue);
+  //       }
+  //     };
+  //   }
+  //
+  //   if($this->stopWorker) {
+  //     $this->stopWorkerHooks[] = $this->stopWorker;
+  //   }
+  // }
+  //
+  // public static function timer($interval, $callBack, $args = [], $persistent = true) {
+  //   /*
+  //   * 调用php页面运行时间检测类，定时执行某个函数或者类方法。
+  //   * @param $interval   执行间隔
+  //   * @param $callback   执行函数，若是类的方法，必须是类的公有方法
+  //   * @param $args       回调函数的参数，必须是数组
+  //   * @param $persistent 是否持久，若只想执行一次则传false,只执行一次的任务在执行完毕后会自动销毁，不必调用Timer::del()
+  //   */
+  //   reuturn Timer::add($interval, $callBack, $args, $persistent);
+  // }
+  //
+  // public static function timerDel($timer_id) {
+  //   Timer::del($timer_id);
+  // }
+  //
+  // public static function run() {
+  //   Worker::runAll();
+  // }
+  //
+  // //命令行控制
+  // public function command() {
+  //   switch($this->commands[1]) {
+  //     case 'start' :
+  //       foreach($this->seed as $url) {
+  //         if(is_string($url)) {
+  //           $this->queue()->add($url);
+  //         } elseif(is_array($url)) {
+  //           $this->queue()->add($url[0], $url[1]);
+  //         }
+  //       }
+  //       $this->queues = null;//初始化
+  //       $this->log('Spide is starting');
+  //       break;
+  //
+  //     case 'clean':
+  //       $this->queue->clean();
+  //       die();
+  //       break;
+  //     case 'stop':
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
+  //
+  // private function fileLog($msg, $spide) {
+  //   $myFile  = fopen($this->lofFile, "a") or die ('open file failed!');
+  //   $content = date('Y-m-d H:i:s')."$spide->name : $msg\n";
+  //   fwrite($myFile, $content);
+  //   fclose($myFile);
+  // }
+  //
+  // public function log($msg) {
+  //   call_user_func($this->logFactory, $msg, $this);
+  // }
+  //
+  // public function setLog($callBack = null) {
+  //   $this->logFactory = $callBack === null ? function($msg, $spide) {
+  //     echo date('Y-m-d H:i:s')."$spide->name : $msg\n";
+  //   } : $callBack;
+  // }
+  //
+  // //非守护进程下执行，一次爬取五条
+  // public function crawler() {
+  //   $allHooks = $this->hooks;
+  //   array_shift($allHooks);
+  //   array_pop($allHooks);
+  //
+  //   foreach ($allHooks as $hooks) {//爬取5条记录
+  //       foreach ($this->$hooks as $hook) {//完成爬取一条记录的整个流程
+  //           call_user_func($hook, $this);
+  //       }
+  //   }
+  //
+  //   // $this->queue = '';
+  //   // $this->url = '';
+  //   // $this->method = '';
+  //   // $this->page = '';
+  //   // $this->options = [];
+  // }
 
   // public function defaultBeforeDownloadPage() {
   //   if($this->daemonize) {
