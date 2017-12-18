d1b425fb0ca9c95b700bfe55fa3c670871707080
ullone <ullone@163.com>
2017-12-18 14:50:33 +0800

    fix

--- ams/public/spide/spide.php
+++ ams/public/spide/spide.php
@@ -67,34 +67,34 @@ class Spide {
 
   //爬虫启动函数
   public function start() {
-    // if(!isset($this->commands)) {
-    //   $this->daemonize = false;
-    // }
+    if(!isset($this->commands)) {
+      $this->daemonize = false;
+    }
 
     if($this->daemonize) {
       //启动守护进程
-      // $this->check();
+      $this->check();
 
-      // $worker = new Worker;
-      // $worker->count = $this->count;
-      // $worker->name  = $this->name;//worker实例名称
-      // $worker->onWorkerStart = [$this, 'onWorkerStart'];
-      // $worker->onWorkerStop  = [$this, 'onWorkerStop'];
-      // $this->worker = $worker;
+      $worker = new Worker;
+      $worker->count = $this->count;
+      $worker->name  = $this->name;//worker实例名称
+      $worker->onWorkerStart = [$this, 'onWorkerStart'];
+      $worker->onWorkerStop  = [$this, 'onWorkerStop'];
+      $this->worker = $worker;
 
-      // Worker::$daemonize = true;
-      // Worker::$stdoutFile = $this->logFile;
-      //
-      // $this->queueArgs['name'] = $this->name;
-      // $this->initHooks();
-      // $this->command();
-      // self::run();
+      Worker::$daemonize = true;
+      Worker::$stdoutFile = $this->logFile;
+
+      $this->queueArgs['name'] = $this->name;
+      $this->initHooks();
+      $this->command();
+      self::run();
     } else {
-      // $this->initHooks();
-      // $this->seed = (array) $this->seed;
-      // while (count($this->seed)) {
-          // $this->crawler();
-      // }
+      $this->initHooks();
+      $this->seed = (array) $this->seed;
+      while (count($this->seed)) {
+          $this->crawler();
+      }
     }
   }
 
@@ -113,63 +113,63 @@ class Spide {
   // }
 
   //初始化一系列钩子函 数并规定执行顺序
-  // public function initHooks() {
-  //   //写日志
-  //   $this->startWorkerHooks[] = function ($spide) {
-  //       $spide->id = $spide->worker->id;
-  //       $spide->log("Beanbun worker {$spide->id} is starting ...");
-  //   };
-  //
-  //   //自定义处理函数
-  //   if ($this->startWorker) {
-  //       $this->startWorkerHooks[] = $this->startWorker;
-  //   }
-  //   //设置队列大小
-  //   $this->startWorkerHooks[] = function ($spide) {
-  //       $spide->queue()->maxQueueSize = $spide->max;
-  //       //定时调用一次$this->hooks里的函数，一次爬取五条url
-  //       $spide->timer_id = Spide::timer($spide->interval, [$spide, 'crawler']);
-  //   };
-  //
-  //   $this->beforeDownloadPageHooks[] = [$this, 'defaultBeforeDownloadPage'];
-  //
-  //   if($this->beforeDownloadPage) {
-  //     $this->beforeDownloadPageHooks[] = $this->beforeDownloadPage;
-  //   }
-  //
-  //   if($this->downloadPage) {
-  //     $this->downloadPageHooks[] = $this->downloadPage;
-  //   } else {
-  //     $this->downloadPageHooks[] = [$this, 'defaultDownloadPage'];
-  //   }
-  //
-  //   if($this->afterDownloadPage) {
-  //     $this->afterDownloadPageHooks[] = $this->afterDownloadPage;
-  //   }
-  //
-  //   if($this->discoverUrl) {
-  //     $this->discoverUrlHooks[] = $this->discoverUrl;
-  //   } else {
-  //     $this->discoverUrlHooks[] = [$this, 'defaultDiscoverUrl'];
-  //   }
-  //
-  //   if($this->afterDiscover) {
-  //     $this->afterDiscoverHooks[] = $this->afterDiscover;
-  //   }
-  //
-  //   if($this->daemonize) {
-  //     $this->afterDiscoverHooks[] = function ($spide) {
-  //       if($spide->options['reserve'] == false) {
-  //         $spide->queue()->queued($spide->queue);
-  //       }
-  //     };
-  //   }
-  //
-  //   if($this->stopWorker) {
-  //     $this->stopWorkerHooks[] = $this->stopWorker;
-  //   }
-  // }
-  //
+  public function initHooks() {
+    //写日志
+    $this->startWorkerHooks[] = function ($spide) {
+        $spide->id = $spide->worker->id;
+        $spide->log("Beanbun worker {$spide->id} is starting ...");
+    };
+
+    //自定义处理函数
+    if ($this->startWorker) {
+        $this->startWorkerHooks[] = $this->startWorker;
+    }
+    //设置队列大小
+    $this->startWorkerHooks[] = function ($spide) {
+        $spide->queue()->maxQueueSize = $spide->max;
+        //定时调用一次$this->hooks里的函数，一次爬取五条url
+        $spide->timer_id = Spide::timer($spide->interval, [$spide, 'crawler']);
+    };
+
+    $this->beforeDownloadPageHooks[] = [$this, 'defaultBeforeDownloadPage'];
+
+    if($this->beforeDownloadPage) {
+      $this->beforeDownloadPageHooks[] = $this->beforeDownloadPage;
+    }
+
+    if($this->downloadPage) {
+      $this->downloadPageHooks[] = $this->downloadPage;
+    } else {
+      $this->downloadPageHooks[] = [$this, 'defaultDownloadPage'];
+    }
+
+    if($this->afterDownloadPage) {
+      $this->afterDownloadPageHooks[] = $this->afterDownloadPage;
+    }
+
+    if($this->discoverUrl) {
+      $this->discoverUrlHooks[] = $this->discoverUrl;
+    } else {
+      $this->discoverUrlHooks[] = [$this, 'defaultDiscoverUrl'];
+    }
+
+    if($this->afterDiscover) {
+      $this->afterDiscoverHooks[] = $this->afterDiscover;
+    }
+
+    if($this->daemonize) {
+      $this->afterDiscoverHooks[] = function ($spide) {
+        if($spide->options['reserve'] == false) {
+          $spide->queue()->queued($spide->queue);
+        }
+      };
+    }
+
+    if($this->stopWorker) {
+      $this->stopWorkerHooks[] = $this->stopWorker;
+    }
+  }
+
   // public static function timer($interval, $callBack, $args = [], $persistent = true) {
   //   /*
   //   * 调用php页面运行时间检测类，定时执行某个函数或者类方法。
@@ -185,36 +185,36 @@ class Spide {
   //   Timer::del($timer_id);
   // }
   //
-  // public static function run() {
-  //   Worker::runAll();
-  // }
+  public static function run() {
+    Worker::runAll();
+  }
 
   //命令行控制
-  // public function command() {
-  //   switch($this->commands[1]) {
-  //     case 'start' :
-  //       foreach($this->seed as $url) {
-  //         if(is_string($url)) {
-  //           $this->queue()->add($url);
-  //         } elseif(is_array($url)) {
-  //           $this->queue()->add($url[0], $url[1]);
-  //         }
-  //       }
-  //       $this->queues = null;//初始化
-  //       $this->log('Spide is starting');
-  //       break;
-  //
-  //     case 'clean':
-  //       $this->queue->clean();
-  //       die();
-  //       break;
-  //     case 'stop':
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
-  //
+  public function command() {
+    switch($this->commands[1]) {
+      case 'start' :
+        foreach($this->seed as $url) {
+          if(is_string($url)) {
+            $this->queue()->add($url);
+          } elseif(is_array($url)) {
+            $this->queue()->add($url[0], $url[1]);
+          }
+        }
+        $this->queues = null;//初始化
+        $this->log('Spide is starting');
+        break;
+
+      case 'clean':
+        $this->queue->clean();
+        die();
+        break;
+      case 'stop':
+        break;
+      default:
+        break;
+    }
+  }
+
   // private function fileLog($msg, $spide) {
   //   $myFile  = fopen($this->lofFile, "a") or die ('open file failed!');
   //   $content = date('Y-m-d H:i:s')."$spide->name : $msg\n";
@@ -232,25 +232,25 @@ class Spide {
   //   } : $callBack;
   // }
   //
-  // //非守护进程下执行，一次爬取五条
-  // public function crawler() {
-  //   $allHooks = $this->hooks;
-  //   array_shift($allHooks);
-  //   array_pop($allHooks);
-  //
-  //   foreach ($allHooks as $hooks) {//爬取5条记录
-  //       foreach ($this->$hooks as $hook) {//完成爬取一条记录的整个流程
-  //           call_user_func($hook, $this);
-  //       }
-  //   }
-  //
-  //   // $this->queue = '';
-  //   // $this->url = '';
-  //   // $this->method = '';
-  //   // $this->page = '';
-  //   // $this->options = [];
-  // }
-  //
+  //非守护进程下执行，一次爬取五条
+  public function crawler() {
+    $allHooks = $this->hooks;
+    array_shift($allHooks);
+    array_pop($allHooks);
+
+    foreach ($allHooks as $hooks) {//爬取5条记录
+        foreach ($this->$hooks as $hook) {//完成爬取一条记录的整个流程
+            call_user_func($hook, $this);
+        }
+    }
+
+    // $this->queue = '';
+    // $this->url = '';
+    // $this->method = '';
+    // $this->page = '';
+    // $this->options = [];
+  }
+
   // public function defaultBeforeDownloadPage() {
   //   if($this->daemonize) {
   //     if($this->max > 0 && $this->queue()->queuedCount() >= $this->max) {
@@ -345,49 +345,49 @@ class Spide {
   //   $this->queueArgs = $arr;
   // }
 
-  // public function check()
-  // {
-  //     $error = false;
-  //     $text = '';
-  //     $version_ok = $pcntl_loaded = $posix_loaded = true;
-  //     if (!version_compare(phpversion(), "5.3.3", ">=")) {
-  //         $text .= "PHP Version >= 5.3.3                 \033[31;40m [fail] \033[0m\n";
-  //         $error = true;
-  //     }
-  //
-  //     if (!in_array("pcntl", get_loaded_extensions())) {
-  //         $text .= "Extension posix check                \033[31;40m [fail] \033[0m\n";
-  //         $error = true;
-  //     }
-  //
-  //     if (!in_array("posix", get_loaded_extensions())) {
-  //         $text .= "Extension posix check                \033[31;40m [fail] \033[0m\n";
-  //         $error = true;
-  //     }
-  //
-  //     $check_func_map = array(
-  //         "stream_socket_server",
-  //         "stream_socket_client",
-  //         "pcntl_signal_dispatch",
-  //     );
-  //
-  //     if ($disable_func_string = ini_get("disable_functions")) {
-  //         $disable_func_map = array_flip(explode(",", $disable_func_string));
-  //     }
-  //
-  //     foreach ($check_func_map as $func) {
-  //         if (isset($disable_func_map[$func])) {
-  //             $text .= "\033[31;40mFunction " . implode(', ', $check_func_map) . "may be disabled. Please check disable_functions in php.ini\033[0m\n";
-  //             $error = true;
-  //             break;
-  //         }
-  //     }
-  //
-  //     if ($error) {
-  //         echo $text;
-  //         exit;
-  //     }
-  // }
+  public function check()
+  {
+      $error = false;
+      $text = '';
+      $version_ok = $pcntl_loaded = $posix_loaded = true;
+      if (!version_compare(phpversion(), "5.3.3", ">=")) {
+          $text .= "PHP Version >= 5.3.3                 \033[31;40m [fail] \033[0m\n";
+          $error = true;
+      }
+
+      if (!in_array("pcntl", get_loaded_extensions())) {
+          $text .= "Extension posix check                \033[31;40m [fail] \033[0m\n";
+          $error = true;
+      }
+
+      if (!in_array("posix", get_loaded_extensions())) {
+          $text .= "Extension posix check                \033[31;40m [fail] \033[0m\n";
+          $error = true;
+      }
+
+      $check_func_map = array(
+          "stream_socket_server",
+          "stream_socket_client",
+          "pcntl_signal_dispatch",
+      );
+
+      if ($disable_func_string = ini_get("disable_functions")) {
+          $disable_func_map = array_flip(explode(",", $disable_func_string));
+      }
+
+      foreach ($check_func_map as $func) {
+          if (isset($disable_func_map[$func])) {
+              $text .= "\033[31;40mFunction " . implode(', ', $check_func_map) . "may be disabled. Please check disable_functions in php.ini\033[0m\n";
+              $error = true;
+              break;
+          }
+      }
+
+      if ($error) {
+          echo $text;
+          exit;
+      }
+  }
 
   // public function shutdown()
   // {
